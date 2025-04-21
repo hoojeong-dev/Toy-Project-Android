70769aab5be4d4ce383153e5995ca239bf48eeb2
김후정 <fujeong00@gmail.com>
2025-04-21 14:27:48 +0900

    chore: Wearable-ClassicBluetooth 프로젝트 추가

---
+++ WearableClassicBluetooth/wearable/src/main/kotlin/com/example/wearable/classicbluetooth/ui/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.example.wearable.classicbluetooth.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun WearableClassicBluetoothTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
